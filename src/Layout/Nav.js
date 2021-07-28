f8ec353506a8128333052f73c079a547a2cfae23
Joseph Zhu <josephzhu3@gmail.com>
2021-07-28 13:08:13 -0700

    download bootstrap and update Contact

--- src/Layout/Nav.js
+++ src/Layout/Nav.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Nav() {
     return (
-        <div>
-            Nav Bar Connected
-        </div>
+        <nav>
+            <div>
+                <Link>
+                    About
+                </Link>
+                <Link>
+                    Projects
+                </Link>
+                <Link>
+                    Contact
+                </Link>
+            </div>
+        </nav>
     )
 }
