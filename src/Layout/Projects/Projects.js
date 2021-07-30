f32ba04dc4c64f53bdae581c6b7126f67bf1f69c
Joseph Zhu <josephzhu3@gmail.com>
2021-07-30 12:54:14 -0700

    Updating data and connecting to data storage

--- src/Layout/Projects/Projects.js
+++ src/Layout/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import project from "../../DataStorage/ProjectPics.json"
 
 
 export default function Projects() {
@@ -8,9 +8,10 @@ export default function Projects() {
             <h3>RAM</h3>
             <h4>Restaurant Assistance Manager</h4>
             <p>
-            {/* <img src="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA" alt="test"/> */}
+                {project[0].name}
             </p>
             <a alt="test" href="https://restaurant-reservation-front-end-opal.vercel.app/dashboard"> 
+            <img alt="App" src="https://lh3.googleusercontent.com/jXSIhGC3vEjhVj6otz5CRDYl94NHkSspSORmFOCkpB7t_N3ECRYwnYo2FFQFx0ywKePTiEZwNkVu14XxCUNEOaPshs-2Yy-O8yHKE14IE2I2O9ZjnLUNoWUf_ez2oZWB6Ie8Kklp1w=w2400" />
             {/* <img alt="App" src="https://lh3.googleusercontent.com/VZooqI-XBZywGKntPfMVGZWDxMZgflc5knifp047XYGY7zdjLlgD56ZUizOC7OqVZZuKxDWVyLKrr-E-96I2y2kVUefs7FhKLHI3UMfLuEPohjbcZy7-AvSb1lsGpoeabx6KnGzJFg=w600-h315-p-k" /> */}
             </a>
             {/* <a href="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA">Photo Link Test</a> */}
