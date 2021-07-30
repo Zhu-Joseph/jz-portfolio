9f870dc33a24431019eaafe52ae80df62c54b141
Joseph Zhu <josephzhu3@gmail.com>
2021-07-30 11:20:53 -0700

    Create data storage files

--- src/Layout/Projects/Projects.js
+++ src/Layout/Projects/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 export default function Projects() {
     return (
         <div>
@@ -9,7 +11,7 @@ export default function Projects() {
             {/* <img src="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA" alt="test"/> */}
             </p>
             <a alt="test" href="https://restaurant-reservation-front-end-opal.vercel.app/dashboard"> 
-            <img alt="App" src="https://lh3.googleusercontent.com/VZooqI-XBZywGKntPfMVGZWDxMZgflc5knifp047XYGY7zdjLlgD56ZUizOC7OqVZZuKxDWVyLKrr-E-96I2y2kVUefs7FhKLHI3UMfLuEPohjbcZy7-AvSb1lsGpoeabx6KnGzJFg=w600-h315-p-k" />
+            {/* <img alt="App" src="https://lh3.googleusercontent.com/VZooqI-XBZywGKntPfMVGZWDxMZgflc5knifp047XYGY7zdjLlgD56ZUizOC7OqVZZuKxDWVyLKrr-E-96I2y2kVUefs7FhKLHI3UMfLuEPohjbcZy7-AvSb1lsGpoeabx6KnGzJFg=w600-h315-p-k" /> */}
             </a>
             {/* <a href="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA">Photo Link Test</a> */}
             <a href="https://restaurant-reservation-front-end-opal.vercel.app/dashboard"> Aye</a>
