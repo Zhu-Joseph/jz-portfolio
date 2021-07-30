e471d2210056549962bbdec8e50ff77614df0f3c
Joseph Zhu <josephzhu3@gmail.com>
2021-07-30 10:46:41 -0700

    updating pictures

--- src/Layout/Projects/Projects.js
+++ src/Layout/Projects/Projects.js
@@ -3,12 +3,15 @@ import React from 'react'
 export default function Projects() {
     return (
         <div>
-            <h3>Restaurant Reservation</h3>
-            <h4>Manage Restaurant Reservations</h4>
+            <h3>RAM</h3>
+            <h4>Restaurant Assistance Manager</h4>
             <p>
-            <img src="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA" alt="test"/>
+            {/* <img src="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA" alt="test"/> */}
             </p>
-            <a href="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA">Photo Link Test</a>
+            <a alt="test" href="https://restaurant-reservation-front-end-opal.vercel.app/dashboard"> 
+            <img alt="App" src="https://lh3.googleusercontent.com/VZooqI-XBZywGKntPfMVGZWDxMZgflc5knifp047XYGY7zdjLlgD56ZUizOC7OqVZZuKxDWVyLKrr-E-96I2y2kVUefs7FhKLHI3UMfLuEPohjbcZy7-AvSb1lsGpoeabx6KnGzJFg=w600-h315-p-k" />
+            </a>
+            {/* <a href="https://photos.app.goo.gl/FPJZbzjqC9kMZycEA">Photo Link Test</a> */}
             <a href="https://restaurant-reservation-front-end-opal.vercel.app/dashboard"> Aye</a>
         </div>
     )
