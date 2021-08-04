fe7dbed11a59937afea69ad1867597ff9c9056fe
Joseph Zhu <josephzhu3@gmail.com>
2021-08-03 18:01:32 -0700

    Include skills applied on project section

--- src/Layout/Projects/Projects.js
+++ src/Layout/Projects/Projects.js
@@ -12,6 +12,8 @@ export default function Projects() {
                 <a alt={project.subtitle} target="_blank" rel="noreferrer" href={project.liveSiteUrl}>
                     <img alt={project.name} src={project.imageUrl} />
                 </a>
+                <p>{project.description}</p>
+                <p>{project.skillsApplied}</p>
             </div>
         )
     })
@@ -19,16 +21,6 @@ export default function Projects() {
     return (
         <div>
             {list}
-
-            {/* <h3>RAM</h3>
-            <h4>Restaurant Assistance Manager</h4>
-            <p>
-                {projects[0].name}
-            </p>
-            <a alt="test" target="_blank" rel="noreferrer" href="https://restaurant-reservation-front-end-opal.vercel.app/dashboard"> 
-            <img alt="App" src="https://lh3.googleusercontent.com/jXSIhGC3vEjhVj6otz5CRDYl94NHkSspSORmFOCkpB7t_N3ECRYwnYo2FFQFx0ywKePTiEZwNkVu14XxCUNEOaPshs-2Yy-O8yHKE14IE2I2O9ZjnLUNoWUf_ez2oZWB6Ie8Kklp1w=w2400" />
-            </a> */}
-           
         </div>
     )
 }
