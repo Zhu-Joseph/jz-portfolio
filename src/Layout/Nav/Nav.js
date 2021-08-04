644437a098dd8d3cbbc22a27d25277a48897ca4c
Joseph Zhu <josephzhu3@gmail.com>
2021-08-04 00:05:25 -0700

    Updating bootstrap for Navbar

--- src/Layout/Nav/Nav.js
+++ src/Layout/Nav/Nav.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 export default function Nav() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <a href="#About">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="#About">
                         About
                     </a>                   
                 </li>
-                <li>
-                    <a href="#Projects">
+                <li className="nav-item">
+                    <a className="nav-link" href="#Projects">
                         Projects
                     </a>                    
                 </li>
-                <li>
-                    <a href="#Contact">
+                <li className="nav-item">
+                    <a className="nav-link" href="#Contact">
                         Contact
                     </a>
                 </li>
             </ul>
-            <h2>Joseph Zhu</h2>
+            <h2 className="text-align-right">Joseph Zhu</h2>
         </nav>
     )
 }
