44f1b3e03de04239d455d0f0aae5941a2abf8e55
Joseph Zhu <josephzhu3@gmail.com>
2021-08-04 13:18:11 -0700

    Continue styling nav-bar

--- src/Layout/Nav/Nav.js
+++ src/Layout/Nav/Nav.js
@@ -1,26 +1,48 @@
 import React from 'react'
+import "./Nav.css"
 
 export default function Nav() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link" href="#About">
-                        About
-                    </a>                   
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#Projects">
-                        Projects
-                    </a>                    
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#Contact">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-            <h2 className="text-align-right">Joseph Zhu</h2>
+        <nav class="navbar fixed-top navbar-light bg-light">
+        {/* // <nav className="Nav"> */}
+            <div className="d-flex flex-row">
+
+                <ul className="navbar-nav d-flex flex-row">
+                    
+                    <div className="p-2">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#About">
+                                About
+                            </a>                   
+                        </li>
+                    </div>
+
+                    <div className="p-2">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#Projects">
+                                Projects
+                            </a>                    
+                        </li>
+                    </div>
+                    
+                    <div className="p-2">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#Contact">
+                                Contact
+                            </a>
+                        </li>
+                    </div>
+
+                    <li className="mr-auto p-2">
+                        <a className="nav-link" href="#Contact">
+                                Joseph Zhu
+                        </a>
+                    </li>
+
+                </ul>
+            
+            </div>
+
         </nav>
     )
 }
