d47bc054764334a2f4574a2a3bc5ec3ae7066765
Joseph Zhu <josephzhu3@gmail.com>
2021-08-14 00:40:28 -0700

    Update CSS

--- src/Layout/Intro/Intro.js
+++ src/Layout/Intro/Intro.js
@@ -26,7 +26,6 @@ export default function Intro() {
                             However, as I was working in tax, 
                             I eventually realized that building better and more accessible software was going to reach more people than a single CPA could.
                         </p>
-
                         <p>
                             Being a CPA in Los Angeles has given me the background and knowledge of what professionals and average Joes are looking for. 
                             They want something simple, easy to use, and accurate in order to bring them confidence in their financial situation.
